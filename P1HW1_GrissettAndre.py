9b5f3a8a592b6676775682fdf1336a24874c6328
AndreGrizz <99222685+AndreGrizz@users.noreply.github.com>
2022-02-07 19:16:23 -0500

    Third Submission
    Third Submission

---
+++ P1HW1_GrissettAndre.py
@@ -0,0 +1,8 @@
+userNum1 = int(input())
+userNum2 = int(input())
+userNumSquared = userNum1 + userNum2
+userNumMult = userNum1 * userNum2
+print(userNum1)
+print(userNum2)
+print(userNumSquared)
+print(userNumMult)
