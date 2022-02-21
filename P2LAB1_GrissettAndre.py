bdb36b8f26edd53982c568ff4194692afd43cbae
AndreGrizz <99222685+AndreGrizz@users.noreply.github.com>
2022-02-21 18:34:45 -0500

    5th Submission
    5th Submission

---
+++ P2LAB1_GrissettAndre.py
@@ -0,0 +1,15 @@
+user_int = int(input('Enter integer (32 - 126):\n'))
+user_float = float(input('Enter float:\n'))
+userchar = input('Enter character:\n')
+userstr = input('Enter string:\n')
+
+
+# FIXME (1): Finish reading other items into variables, then output the four values on a single line separated by a space
+print(user_int,user_float,userchar,userstr)
+print(userstr,userchar,user_float,user_int)
+print(user_int, 'converted to a character is',chr(user_int))
+
+
+# FIXME (2): Output the four values in reverse
+   
+# FIXME (3): Convert the integer to a characer, and output that character
